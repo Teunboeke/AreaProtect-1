a30b41471a9f0aa679e7693497333239ccc57c46
Philip Shilling <comedyman937@yahoo.com>
2014-06-18 09:13:25 -0400

    Basic MySQL

---
+++ AreaProtect/resources/mysql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS areaprotect_data (
+  owner VARCHAR(16),
+  members VARCHAR(80),
+  flags VARCHAR(75)
+  x1 INT,
+  y1 INT,
+  z1 INT,
+  x2 INT,
+  y2 INT,
+  z2 INT,
+  pvp TINYINT(1),
+  build TINYINT(1),
+  destroy TINYINT(1)
+);
