aa3665f86a6a014f373ecb67ee19a749128f19cb
Philip Shilling <comedyman937@yahoo.com>
2014-06-18 09:18:27 -0400

    Basic SQLite3

---
+++ AreaProtect/resources/sqlite3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE areaprotect (
+  owner TEXT,
+  members TEXT,
+  x1 INTEGER,
+  y1 INTEGER,
+  z1 INTEGER,
+  x2 INTEGER,
+  y2 INTEGER,
+  z2 INTEGER,
+  pvp INTEGER,
+  build INTEGER,
+  destroy INTEGER
+);
