45b8c3a89911dc5af0f92b8c8a935f0aefe1ebb3
Philip Shilling <comedyman937@yahoo.com>
2014-06-29 17:04:03 -0700

    Server doesn't crash! :D

---
+++ AreaProtect/src/AreaProtect/provider/DataProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AreaProtect\provider;
+
+use pocketmine\utils\Config;
+use AreaProtect\Main;
+
+interface DataProvider{
+	public function __construct(AreaProtect $plugin);
+
+	public function createArea($owner, $name, $x1, $y1, $z1, $x2, $y2, $z2, $pvp, $build, $destroy);
+
+	public function checkExists($area);
+
+	public function checkOwner($area);
+
+	public function deleteArea($area);
+
+	public function checkPVP($area);
+
+	public function enablePVP($area);
+	
+	public function disablePVP($area);
+	
+	public function checkBuild($area);
+	
+	public function enableBuild($area);
+	
+	public function disableBuild($area);
+	
+	public function checkDestroy($area);
+	
+	public function enableDestroy($area);
+	
+	public function disableDestroy($area);
+	
+	public function getAllPVP();
+	
+	public function getAllBuild();
+	
+	public function getAllDestroy();
+
+	public function close();
+}
