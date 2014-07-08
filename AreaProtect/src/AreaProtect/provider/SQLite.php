b518083e592437b2e4334f5aaa5aa0e1a97aa888
Philip Shilling <comedyman937@yahoo.com>
2014-07-08 08:51:50 -0700

    Begin Change

---
+++ AreaProtect/src/AreaProtect/provider/SQLite.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AreaProtect\provider;
+
+use pocketmine\plugin\PluginLoader;
+
+use AreaProtect\Main;
+use AreaProtect\provider\DataProvider;
+
+class SQLite implements DataProvider{
+    public function createArea($owner, $name, $x1, $y1, $z1, $x2, $y2, $z2, $pvp, $build, $destroy){
+        
+    }
+
+	public function checkExists($area){
+	    
+	}
+
+	public function checkOwner($area){
+	    
+	}
+
+	public function deleteArea($area){
+	    
+	}
+
+	public function checkPVP($area){
+	    
+	}
+
+	public function enablePVP($area){
+	    
+	}
+
+	public function disablePVP($area){
+	    
+	}
+
+	public function checkBuild($area){
+	    
+	}
+
+	public function enableBuild($area){
+	    
+	}
+
+	public function disableBuild($area){
+	    
+	}
+
+	public function checkDestroy($area){
+	    
+	}
+
+	public function enableDestroy($area){
+	    
+	}
+
+	public function disableDestroy($area){
+	    
+	}
+
+	public function checkInteract($area){
+	    
+	}
+
+	public function enableInteract($area){
+	    
+	}
+
+	public function disableInteract($area){
+	    
+	}
+
+	public function getAllPVP(){
+	    
+	}
+
+	public function getAllBuild(){
+	    
+	}
+
+	public function getAllDestroy(){
+	    
+	}
+
+	public function getAllInteract(){
+	    
+	}
+
+	public function getPositions($area){
+	    
+	}
+
+	public function close(){
+	    
+	}
+}
